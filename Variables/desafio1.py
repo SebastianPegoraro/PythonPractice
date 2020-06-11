2f1bc0d005c4bc291b60f2285eaee147bf285bc7
SebastianPegoraro <sebapego18@gmail.com>
2020-06-11 13:27:58 -0300

    Desafio de Decicion 1

---
+++ Variables/desafio1.py
@@ -0,0 +1,7 @@
+#anios = int(input("Ingrese años \n"))
+if int(input("Ingrese años \n")) >= 10:
+    print("Por favor solicite revisión de suelos en su plantación")
+else:
+    print("Intentaremos ayudarte con un nuevo sistema de control de plagas, y cuidaremos el suelo de tu plantación")
+
+input()
